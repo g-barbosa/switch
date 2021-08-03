ad3b43a7e726d54f8184b3d054f846cb0694ed30
g-barbosa <giovanne.bbarbosa@gmail.com>
2021-08-02 23:03:26 -0300

    fix: add heroku app domain

--- public/switch.js
+++ public/switch.js
@@ -1,4 +1,4 @@
-var socket = io('http://localhost:3000')
+var socket = io('https://just-a-toggle-switch.herokuapp.com/')
 
 function changeState(state) {
   document.getElementById('toggleButton').checked = state;
